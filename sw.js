800abd4c2b9d195a1ac330b71487df78bdb22c9c
kapkbssamarinda <zaidanjauharicenter@gmail.com>
2026-07-06 18:29:33 +0800

    Update CACHE_VERSION to 'kbs-v6' for service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Naikkan CACHE_VERSION setiap kali merilis perubahan file statis.
  * ================================================================ */
 
-const CACHE_VERSION = 'kbs-v5';
+const CACHE_VERSION = 'kbs-v6';
 const PRECACHE = [
   './',
   'index.html',
